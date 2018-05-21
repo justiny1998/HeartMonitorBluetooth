cddacf8bb9973de6f61e107f6cb40f12f4fce434
Young, Justin <justin.kai.young@gmail.com>
2018-05-21 16:04:57 -0500

    finished

--- HeartRateMonitor/HRMViewController.swift
+++ HeartRateMonitor/HRMViewController.swift
@@ -5,26 +5,28 @@
 //  Created by Young, Justin on 5/21/18.
 //  Copyright © 2018 Young, Justin. All rights reserved.
 //
-
 import UIKit
 import CoreBluetooth
+
 let heartRateServiceCBUUID = CBUUID(string: "0x180D")
+let heartRateMeasurementCharacteristicCBUUID = CBUUID(string: "2A37")
+let bodySensorLocationCharacteristicCBUUID = CBUUID(string: "2A38")
 
 class HRMViewController: UIViewController {
-
+  
   @IBOutlet weak var heartRateLabel: UILabel!
   @IBOutlet weak var bodySensorLocationLabel: UILabel!
+  
   var centralManager: CBCentralManager!
   var heartRatePeripheral: CBPeripheral!
-
+  
   override func viewDidLoad() {
-    centralManager = CBCentralManager(delegate: self, queue: nil)
     super.viewDidLoad()
-
-    // Make the digits monospaces to avoid shifting when the numbers change
+    
+    centralManager = CBCentralManager(delegate: self, queue: nil)
     heartRateLabel.font = UIFont.monospacedDigitSystemFont(ofSize: heartRateLabel.font!.pointSize, weight: .regular)
   }
-
+  
   func onHeartRateReceived(_ heartRate: Int) {
     heartRateLabel.text = String(heartRate)
     print("BPM: \(heartRate)")
@@ -33,7 +35,6 @@ class HRMViewController: UIViewController {
 
 extension HRMViewController: CBCentralManagerDelegate {
   func centralManagerDidUpdateState(_ central: CBCentralManager) {
-    //default
     switch central.state {
     case .unknown:
       print("central.state is .unknown")
@@ -51,20 +52,17 @@ extension HRMViewController: CBCentralManagerDelegate {
     }
   }
   
-  func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+  func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                      advertisementData: [String : Any], rssi RSSI: NSNumber) {
     print(peripheral)
-    //Set a reference to the located HM peripheral
     heartRatePeripheral = peripheral
     heartRatePeripheral.delegate = self
-    //Stop scanning once HM peripheral is found
     centralManager.stopScan()
-    //Connect to HM peripheral
     centralManager.connect(heartRatePeripheral)
   }
   
-  //Prints "Connection Successful" if connected to GDO
   func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-    print("Connection Successful.")
+    print("Connected!")
     heartRatePeripheral.discoverServices([heartRateServiceCBUUID])
   }
 }
@@ -78,11 +76,63 @@ extension HRMViewController: CBPeripheralDelegate {
     }
   }
   
-  func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService,
-                  error: Error?) {
+  func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
     guard let characteristics = service.characteristics else { return }
+    
     for characteristic in characteristics {
       print(characteristic)
+      
+      if characteristic.properties.contains(.read) {
+        print("\(characteristic.uuid): properties contains .read")
+        peripheral.readValue(for: characteristic)
+      }
+      if characteristic.properties.contains(.notify) {
+        print("\(characteristic.uuid): properties contains .notify")
+        peripheral.setNotifyValue(true, for: characteristic)
+      }
+    }
+  }
+  
+  func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+    switch characteristic.uuid {
+    case bodySensorLocationCharacteristicCBUUID:
+      let bodySensorLocation = bodyLocation(from: characteristic)
+      bodySensorLocationLabel.text = bodySensorLocation
+    case heartRateMeasurementCharacteristicCBUUID:
+      let bpm = heartRate(from: characteristic)
+      onHeartRateReceived(bpm)
+    default:
+      print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+    }
+  }
+  
+  private func bodyLocation(from characteristic: CBCharacteristic) -> String {
+    guard let characteristicData = characteristic.value,
+      let byte = characteristicData.first else { return "Error" }
+    
+    switch byte {
+    case 0: return "Other"
+    case 1: return "Chest"
+    case 2: return "Wrist"
+    case 3: return "Finger"
+    case 4: return "Hand"
+    case 5: return "Ear Lobe"
+    case 6: return "Foot"
+    default:
+      return "Reserved for future use"
+    }
+  }
+  
+  private func heartRate(from characteristic: CBCharacteristic) -> Int {
+    guard let characteristicData = characteristic.value else { return -1 }
+    let byteArray = [UInt8](characteristicData)
+    let firstBitValue = byteArray[0] & 0x01
+    if firstBitValue == 0 {
+      // Heart Rate Value Format is in the 2nd byte
+      return Int(byteArray[1])
+    } else {
+      // Heart Rate Value Format is in the 2nd and 3rd bytes
+      return (Int(byteArray[1]) << 8) + Int(byteArray[2])
     }
   }
 }
